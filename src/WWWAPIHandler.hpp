bfeaf9accf5b634888801d411c32314ab69b28cf
Tristan Seifert <t-root_fry.lab.tseifert.me@tseifert.me>
2017-01-16 00:20:51 -0600

    Added note on authentication

--- src/WWWAPIHandler.hpp
+++ src/WWWAPIHandler.hpp
@@ -2,6 +2,10 @@
  * Handles API requests for the web API. This will take in a property tree as
  * input (if applicable), then output a property tree. The web server will
  * serialize/unserialize as needed.
+ *
+ * NOTE: The API does absolutely no verification or authentication of the
+ * client. It should probably be served behind some sort of reverse-proxy (such
+ * as nginx) with HTTP authentication to protect it.
  */
 #ifndef WWWAPIHANDLER_H
 #define WWWAPIHANDLER_H
